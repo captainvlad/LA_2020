cbc9d48c6f070c0a4d754a384569194077d5277c
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-05-20 02:57:40 +0300

    Initial commit

--- usage.py
+++ usage.py
@@ -1,3 +1,28 @@
 from general import router_function
 
-router_function( "sample_5.jpg", [2, 1] )
+#Example to call each one of subfunctions
+
+# rotate_image
+# router_function( "sample_5.jpg", [1, 90] )
+# flipper
+# router_function( "sample_5.jpg", [2, -1] )
+# grayscale_filter
+# router_function( "sample_5.jpg", [3] )
+# sepia_filter
+# router_function( "sample_5.jpg", [4] )
+# add_brightness_to_image
+# router_function( "sample_5.jpg", [5, 50, 50, 50] )
+# negative
+# router_function( "sample_5.jpg", [6] )
+# contrast
+# router_function( "sample_5.jpg", [7, 10] )
+# gamma_correction
+# router_function( "sample_5.jpg", [8, 5] )
+# Gaussian_blur
+# router_function( "sample_5.jpg", [9] )
+# jpeg_algorithm
+# router_function( "sample_5.jpg", [10] )
+# svd
+# router_function( "sample_5.jpg", [11] )
+# get_predictions
+# router_function( "sample_5.jpg", [12] )
